b567c4bdb57948435dc90d8d9c19685de2468752
kuku0527 <ddsd3931@gmail.com>
2026-04-30 23:34:58 +0800

    Update run_demo.py

--- examples/run_demo.py
+++ examples/run_demo.py
@@ -1,1 +1,7 @@
+from orchestrator.orchestrator import DecisionOrchestratorAgent
+import pprint
 
+if __name__ == "__main__":
+    orchestrator = DecisionOrchestratorAgent()
+    result = orchestrator.run_decision_pipeline()
+    pprint.pprint(result)
